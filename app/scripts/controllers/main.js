b46f9204e1622c3d4216c6130101b14b7c47c188
miro <miro.sorja@gmail.com>
2014-05-05 15:12:18 +0300

    Initialized repository, added foundation framework, did general overlay

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var app = angular.module('beardedNinjaApp');
+app.controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+});
