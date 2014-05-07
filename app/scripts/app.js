3aad46aa90e083ab9214a721f989165d36626bc4
miro <miro.sorja@gmail.com>
2014-05-07 16:30:39 +0300

    @heroku

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,28 +1,28 @@
 'use strict';
 
 angular
-  .module('beardedNinjaApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute',
-    'infinite-scroll'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/ladder', {
-        templateUrl: 'views/ladder.html',
-        controller: 'MainCtrl'
-      })
-      .when('/photo/:id', {
-        templateUrl: 'views/photo.html',
-        controller: 'PhotoCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+.module('beardedNinjaApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'infinite-scroll'
+])
+.config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/main.html',
+    controller: 'MainCtrl'
+  })
+  .when('/photo/:id', {
+    templateUrl: 'views/photo.html',
+    controller: 'PhotoCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
   });
+})
+.config(function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
