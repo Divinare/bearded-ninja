45553c891564bbf3a068b1ccc7f7b63c57eea32b
miro <miro.sorja@gmail.com>
2014-05-07 13:45:12 +0300

    Added infite scroll, fixed image generating

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,8 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
+    'ngRoute',
+    'infinite-scroll'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,17 +2,25 @@
 
 var app = angular.module('beardedNinjaApp');
 
-app.controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+app.factory('Data', function(){
+	var data = {};
+	var URL = 'http://placehold.it/450x350&text=1337';
+	var i = 0;
 
-    $scope.urls = [
-      {name: "http://placehold.it/450x350&text=randomgenid"},
-      {name: "http://placehold.it/450x350&text=randomgenid"},
-      {name: "http://placehold.it/450x350&text=randomgenid"}
-    ];
+	data.gen = function(){
+		return URL + i++;
+	};
+
+	return data;
+});
+
+
+app.controller('MainCtrl', function ($scope, Data) {
+
+	$scope.urls = [Data.gen(), Data.gen(), Data.gen()];
+
+	$scope.scroll = function scroll () {
+		$scope.urls.push(Data.gen());
+	}
 
 });
