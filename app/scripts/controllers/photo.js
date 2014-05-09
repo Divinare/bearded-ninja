0053b0bdf94b5523d88c49c1a876bbf93c62c739
miro <miro.sorja@gmail.com>
2014-05-09 15:08:34 +0300

    Cleaned up a little

--- app/scripts/controllers/photo.js
+++ app/scripts/controllers/photo.js
@@ -2,7 +2,6 @@
 
 angular.module('beardedNinjaApp')
 .controller('PhotoCtrl', function ($scope, $routeParams, $http) {
-	var data = {};
 
 	$http.get('http://bearded-ninja-backend.herokuapp.com/images/' + $routeParams.id + '.json').success( function(data, status, headers, config) {
 		console.log(data);
@@ -10,9 +9,7 @@ angular.module('beardedNinjaApp')
 		console.log(data.name);
 		$scope.image = data;
 
-		$scope.image.tags = [
-			'hello ', 'wrold', 'hell2o ', 'wrol1d', 'hello4 ', 'wro2ld', 
-		];
+		$scope.image.tags = ['as ', 'd', 'a ', 's', 'wer ', 'gfd'];
 
 	});
 
