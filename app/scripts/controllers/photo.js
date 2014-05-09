851cc40452d0c90ada028bfa9a3aeab61ca12e69
miro <miro.sorja@gmail.com>
2014-05-09 14:58:11 +0300

    Fixed images, added static tags - to be fixed

--- app/scripts/controllers/photo.js
+++ app/scripts/controllers/photo.js
@@ -2,21 +2,19 @@
 
 angular.module('beardedNinjaApp')
 .controller('PhotoCtrl', function ($scope, $routeParams, $http) {
+	var data = {};
+
+	$http.get('http://bearded-ninja-backend.herokuapp.com/images/' + $routeParams.id + '.json').success( function(data, status, headers, config) {
+		console.log(data);
+		console.log($routeParams.id);
+		console.log(data.name);
+		$scope.image = data;
+
+		$scope.image.tags = [
+			'hello ', 'wrold', 'hell2o ', 'wrol1d', 'hello4 ', 'wro2ld', 
+		];
+
+	});
 
 
-	   $http.get('http://bearded-ninja-backend.herokuapp.com/images/' + $routeParams.id + '.json').success( function(data, status, headers, config) {
-       console.log(data)
-       console.log($routeParams.id)
-       console.log(data.name)
-       $scope.entries = data;
-    });
-
-	   	$scope.image = {
-        
-	   	console.log(data.name)
-		name: data.name,
-		url: 'http://placehold.it/450x350/&text={PLACEHOLDER}',
-		id: $routeParams.id
-
-	};
 });
