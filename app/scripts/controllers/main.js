5f4f53609016fa425df63e62d66e0c7fcbb368dd
miro <miro.sorja@gmail.com>
2014-05-10 16:44:32 +0300

    Fixed bug not showing generated images

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,7 +14,7 @@ app.factory('Data', function(){
 	data.gen = function(){
 		var URL = 'http://placehold.it/450x350/' + randcol() + '/' + randcol() + '/&text=[1337';
 
-		return {url: URL + i++ + ']', id: i};
+		return {imageUrl: URL + i++ + ']', id: i};
 	};
 
 	return data;
@@ -26,14 +26,14 @@ app.controller('MainCtrl', function ($scope, Data, $resource, $http) {
 /*		$resource('/api/images').query(); */
 
 	// $scope.images = [Data.gen(), Data.gen(), Data.gen()];
-	$scope.scroll = function scroll () {
-		$scope.images.push(Data.gen());
-	};
 
 	$http.get('http://bearded-ninja-backend.herokuapp.com/api/images.json').success( function(data, status, headers, config) {
 		console.log(data)
 		$scope.images = data;
 	});
+	$scope.scroll = function scroll () {
+		$scope.images.push(Data.gen());
+	};
 
 /*	$scope.createImage = function() {
 		Images.create($scope.image).success(function(data, status, headers, config) {
