da7fcb36ee3cd13c1f34242d8ee6cb7dd44217e1
Joe Niemi <niemijoe@hotmail.com>
2014-05-11 02:53:12 +0300

    comments push to back end & cleaning up views

--- app/scripts/controllers/photo.js
+++ app/scripts/controllers/photo.js
@@ -1,7 +1,8 @@
 'use strict';
 
-angular.module('beardedNinjaApp')
-  .directive('myDraggable', ['$document', function($document) {
+var app = angular.module('beardedNinjaApp');
+  
+  app.directive('myDraggable', ['$document', function($document) {
     return function(scope, element, attr) {
       var startX = 0, startY = 0, x = 0, y = 0;
 
@@ -33,8 +34,9 @@ angular.module('beardedNinjaApp')
         $document.off('mouseup', mouseup);
       }
     };
-  }])
-.controller('PhotoCtrl', function ($scope, $routeParams, $http) {
+  }]);
+
+app.controller('PhotoCtrl', function ($scope, $routeParams, $http, Comments) {
 
 	$http.get('http://bearded-ninja-backend.herokuapp.com/api/images/' + $routeParams.id + '.json').success( function(data, status, headers, config) {
 		console.log(data);
@@ -44,7 +46,42 @@ angular.module('beardedNinjaApp')
 
 		$scope.image.tags = ['as ', 'd', 'a ', 's', 'wer ', 'gfd'];
 
+/*		$scope.createComment = function() {
+        $http.post('http://bearded-ninja-backend.herokuapp.com/comments.json', $scope.comment).success( function(data, status, headers, config) {
+            $scope.entries.push(data)
+        });
+        $scope.comment = {}
+    }*/
+    $scope.createComment = function() {
+     Comments.create($scope.comment).success(function(data, status, headers, config) {
+/*        $scope.entries.push(data);*/
+     });
+     $scope.comment = {}
+    }
+
 	});
 
 
 });
+
+app.factory('Comments', function($http){
+    var URL_BASE = 'http://bearded-ninja-backend.herokuapp.com/comments';
+
+    var commentsService = {};
+    
+    commentsService.all = function(){
+     return $http.get(URL_BASE+".json")	
+    }
+
+    commentsService.create = function(data){
+     console.log("called")
+     return $http.post(URL_BASE+".json", data)	
+    }
+
+    commentsService.delete = function(data){
+      return $http.delete(URL_BASE+"/"+data.id+".json", data)
+    }
+
+    return commentsService;
+});
+
